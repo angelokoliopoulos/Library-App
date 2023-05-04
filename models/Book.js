1a94a15bea79c1bf212e846a30d0edcc60d2b118
[Angelo Kolios] <49371480+koliosor@users.noreply.github.com>
2023-05-05 00:41:20 +0300

    Added user schema

--- models/Book.js
+++ models/Book.js
@@ -24,4 +24,39 @@ const bookSchema = new mongoose.Schema({
 });
 
 const Book = mongoose.model("Book", bookSchema);
-module.exports = Book;
+
+// const UserSchema = new mongoose.Schema({
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+//   books: [
+//     {
+//       title: {
+//         type: String,
+//         required: [true, "must provide a name"],
+//         trim: true,
+//         maxlength: [20, "name can not be more than 20 characters"],
+//       },
+//       author: {
+//         type: String,
+//         required: [true, "must provide an author's name"],
+//         trim: true,
+//         maxlength: [20, "name can not be more than 20 characters"],
+//       },
+//       pages: {
+//         type: Number,
+//         required: true,
+//       },
+//       completed: {
+//         type: Boolean,
+//         default: false,
+//       },
+//     },
+//   ],
+// });
